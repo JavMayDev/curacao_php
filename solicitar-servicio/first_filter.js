415681ff5bbca9552a5363bb21840401192de98a
javi_mac <javimayorque@gmail.com>
2020-11-09 13:07:32 -0600

    adviser users changes, production ready

--- solicitar-servicio/first_filter.js
+++ solicitar-servicio/first_filter.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var coverageInput = document.getElementsByName('coverage').item(0);
     var deliveryDateInput = document.getElementsByName('delivery_date').item(0);
 
+    // add the the next function as event lsitener in those fields
     serviceSelect.addEventListener('change', checkServiceAvailability);
     warranty.addEventListener('change', checkServiceAvailability);
     naCheckbox.addEventListener('change', checkServiceAvailability);
@@ -13,6 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
     deliveryDateInput.addEventListener('change', checkDate);
 
     function checkServiceAvailability() {
+
+	// get day difference between delivery date and now
+        var nowDate = new Date(Date.now());
+        var deliveryDate = new Date(deliveryDateInput.value + 'T00:00:00');
+        var diffTime = Math.abs(nowDate - deliveryDate);
+        var dayDiff = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
         if (naCheckbox.checked) {
             warranty.setAttribute('disabled', true);
             setErrorFeedback(warranty, false);
@@ -25,62 +33,51 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // those values depends of the warranty and time transcurred
         if (
-            serviceSelect.value === 'reclamo' ||
+            // serviceSelect.value === 'reclamo' ||
             serviceSelect.value === 'servicio'
         ) {
-            // if doesn't have warranty and wants to choose 'servicio' or 'reclamo', set error feedback
+            // if doesn't have warranty and wants to choose 'servicio'
+            // set error feedback
             if (!warranty.checked) {
                 setErrorFeedback(
                     serviceSelect,
                     true,
                     'no se puede solicitar este servicio sin garantía'
                 );
-                // return;
             } else setErrorFeedback(serviceSelect, false);
-
-            var nowDate = new Date(Date.now());
-            var deliveryDate = new Date(deliveryDateInput.value + 'T00:00:00');
-
-            var diffTime = Math.abs(nowDate - deliveryDate);
-            var dayDiff = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-            // if product older than 30 days can't choose 'reclamo'
-            if (serviceSelect.value === 'reclamo' && dayDiff >= 30) {
+            // if empty field
+            if (!coverageInput.value)
                 setErrorFeedback(
-                    deliveryDateInput,
+                    coverageInput,
                     true,
-                    'Solo tiene 30 dias para solicitar reclamo'
+                    'Por favor indique la covertura de la garantía'
                 );
-            } else setErrorFeedback(deliveryDateInput, false);
+            else {
+                setErrorFeedback(coverageInput, false);
 
-            if (serviceSelect.value === 'servicio') {
-                // if empty field
-                if (!coverageInput.value)
+                if (coverageInput.value < dayDiff / 365)
                     setErrorFeedback(
                         coverageInput,
                         true,
-                        'Por favor indique la covertura de la garantía'
+                        'La garantía no cubre el tiempo transcurrido'
                     );
-                else {
-                    setErrorFeedback(coverageInput, false);
-
-                    if (coverageInput.value < dayDiff / 365)
-                        setErrorFeedback(
-                            coverageInput,
-                            true,
-                            'La garantía no cubre el tiempo transcurrido'
-                        );
-                    else setErrorFeedback(coverageInput, false);
-                }
-            } else setErrorFeedback(coverageInput, false);
+                else setErrorFeedback(coverageInput, false);
+            }
         } else {
+            // this things can't be wrong if 'service' isn't choosen
+            setErrorFeedback(coverageInput, false);
             setErrorFeedback(serviceSelect, false);
         }
-
-        console.log(
-            'serviceSelect classList contains invalid',
-            serviceSelect.classList.contains('is-invalid')
-        );
+        console.log('day diff just before check reclamo: ', dayDiff);
+        // if product older than 30 days can't choose 'reclamo'
+        if (serviceSelect.value === 'reclamo' && dayDiff >= 30) {
+            console.log('older than 30 days');
+            setErrorFeedback(
+                deliveryDateInput,
+                true,
+                'Solo tiene 30 dias para solicitar reclamo'
+            );
+        } else setErrorFeedback(deliveryDateInput, false);
 
         // if some of the fields that can cause invalidness is
         // in fact, invalid, block all form controls
