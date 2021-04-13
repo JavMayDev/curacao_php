867c99a91549bd119b1dca603ec6b27b992167da
maydev <javimayorque@gmail.com>
2021-04-13 17:06:53 -0500

    get active days for open services on export all

--- exportar/whole_export.php
+++ exportar/whole_export.php
@@ -19,6 +19,11 @@ include(__DIR__.'/../db.php');
 $services = mysqli_query($dbconn, 'SELECT * FROM services;');
 while($service = mysqli_fetch_assoc($services)) {
     $csv_data .= "\n";
+
+    /* get active days for open services */
+    $service['active_days'] = date_diff(date_create($service['service_date']),
+	date_create(date("Y-m-d")))->format("%a");
+
     foreach($fields as $field) {
 	$key = $field['key'];
 	$csv_data .= "\"${service[$key]}\",";
